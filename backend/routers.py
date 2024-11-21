f8b7132855de581fd79c38d3bf82c6db3248193b
isabella <isabellaaguiar.0912@gmail.com>
2024-11-21 19:02:00 -0300

    Terceiro commit

--- backend/routers.py
+++ backend/routers.py
@@ -1,8 +1,29 @@
 import streamlit as st
-from backend.views import pegar_df_planilhao
+import pandas as pd 
+from backend.views import pegar_df_planilhao,filtrar_duplicado,pegar_df_preco_corrigido,pegar_df_preco_diversos, carteira
 
 def menu_planilhao(data_base):
+    # Chama a função para pegar os dados do Planilhão com base na data base.
     df = pegar_df_planilhao(data_base)
-    return df 
+    return df
 
+def menu_filtrar(data_base):
+    # Chama a função remover as duplicadas.
+    df_dupicado = filtrar_duplicado(data_base)
+    return df_dupicado
+
+def menu_estrategia(data, crit_rentabilidade, crit_desconto, num_acoes):
+    # Chama a função para gerar a carteira com base nos critérios informados.
+    df_estrategia = carteira(data, crit_rentabilidade, crit_desconto, num_acoes)
+    return df_estrategia
+
+def menu_preco_corrigido(ticker, data_ini, data_fim):
+    # Chama a função para obter os preços corrigidos das ações para o período.
+    df_precos_corrigidos = pegar_df_preco_corrigido(ticker, data_ini, data_fim)
+    return df_precos_corrigidos
+
+def menu_preco_diversos(ticker, data_ini, data_fim):
+    # Chama a função para obter os preços diversos da ação  para o período.
+    df_precos_diversos = pegar_df_preco_diversos(ticker, data_ini, data_fim)
+    return df_precos_diversos
 
